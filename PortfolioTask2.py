5abe022f2c20aabc97e9c8c5960101dc59e6170c
benferro2003 <74298155+benferro2003@users.noreply.github.com>
2022-12-09 15:25:40 +0000

    Update PortfolioTask2.py

--- PortfolioTask2.py
+++ PortfolioTask2.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
 def Calculate_Output(runners, run_times):
     print(f'\nTotal Runners: {len(runners)}\n')
-   
+
     my_int = len(run_times)
-   
+
     average = sum(run_times) / my_int
-   
+
     fastest = min(run_times)
-   
+
     slowest = max(run_times)
-   
+
     best = run_times.index(fastest)
-   
-    print(f'Average Time:  {average/60:.1f} minutes {average%60:.1f} seconds\n')
-   
-    print(f'Fastest Time: {fastest/60:.1f} minutes {fastest%60:.1f} seconds\n')
-   
-    print(f'Slowest Time: {slowest/60:.1f} minutes {slowest%60:.1f} seconds\n')
-   
-    print(f'Best Time Here: Runner #{runners[best]}')
+
+    print(f'Average Time:  {average / 60:.1f} minutes {average % 60:.1f} seconds\n')
+
+    print(f'Fastest Time: {fastest / 60:.1f} minutes {fastest % 60:.1f} seconds\n')
+
+    print(f'Slowest Time: {slowest / 60:.1f} minutes {slowest % 60:.1f} seconds\n')
+
+    if best < len(runners):
+        print(f'Best Time Here: Runner #{runners[best]}')
+    else:
+        print('Error: best index out of range for runners list')
 
 
 if __name__ == '__main__':
     print('Portfolio task 2 - BEN FERRO')
-    
+
     print(f'\nPark Run Timer\n~~~~~~~~~~~~~~\n')
 
     End = False
@@ -33,30 +36,36 @@ if __name__ == '__main__':
 
     print(f"Let's go!\n ")
     while End == 0:
-        Runner_Num = input("Enter runner number >")
-        Runs.append(Runner_Num)
-        
-        while Runner_Num == "":
-            print('No data found. Nothing to do. What a pity')
-            
-            Runner_Num = input("Enter runner number >")
+        Runner_Num_Run_Time = input("Enter runner number and time separated by '::' >")
+        if "::" in Runner_Num_Run_Time:
+            Runner_Num, Run_Time = Runner_Num_Run_Time.split("::")
 
-        Run_Time = int(input("Enter runner time >"))
-        
-        while Run_Time > 3600 or Run_Time is None:
-            print('Error in data stream Ignoring Carry on')
-            
-            Run_Time = int(input("Enter runner time >"))
+            if Runner_Num.isdigit() and Run_Time.isdigit():
+                Runner_Num = int(Runner_Num)
+                Run_Time = int(Run_Time)
+                Runs.append(Runner_Num)
+            else:
+                print("Error: input must be an integer.")
+
+            if Run_Time > 3600 or Run_Time is None:
+                print('Error in data stream Ignoring Carry on')
+
+        else:
+            print("Error: input must be in the correct format.")
 
         Times.append(Run_Time)
-        
+
         print(f'{Runner_Num}::{Run_Time}')
-        
-        if Runner_Num.upper() == "END" or Run_Time == 0:
+
+        if Runner_Num == 0 or Run_Time == 0:
             End = 1
-            
+
             Runs.remove(Runner_Num)
-            
+
             Times.remove(Run_Time)
-            
+
+        if Runner_Num_Run_Time.lower() == "end":
+            End = 1
+
             Calculate_Output(Runs, Times)
+

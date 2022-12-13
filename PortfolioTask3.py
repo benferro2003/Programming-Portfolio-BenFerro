d09f9fc43337cee94cee673257d28e836fdb5dc2
benferro2003 <74298155+benferro2003@users.noreply.github.com>
2022-12-13 13:13:49 +0000

    Update PortfolioTask3.py

--- PortfolioTask3.py
+++ PortfolioTask3.py
@@ -1,13 +1,26 @@
-from random import*
-import string
-if __name__ == '__main__':
-    print('Portfolio task 3 - BEN FERRO')
+#!/usr/bin/env python3
+from random import *
 
-    with open('students.txt', 'r') as file:
+import string
+
+import sys
+
+import os
+
+if __name__ == '__main__':
+    print(f'Portfolio task 3 - BEN FERRO')
+
+    if not os.path.exists('students.txt'):
+        print(f'Cannot open file Sorry about that.')
+
+        sys.exit(1)
+
+    with open("students.txt", 'r') as file:
         students = file.readlines()
 
     with open('emails.txt', 'w') as file:
         for student in students:
+
             student_number = student[0:8]
 
             first_initial = student[9]
